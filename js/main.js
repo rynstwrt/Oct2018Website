3fe24510a525534b8aca2c23a6bf39c73c80d672
Ryan Stewart <ryanstewartalex@gmail.com>
2020-10-12 19:35:21 -0500

    Add Line Drawing Script

--- js/main.js
+++ js/main.js
@@ -1,39 +1,95 @@
 // Ryan Stewart 10/12/2020
+const transitionendevents = 'transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd';
 $(window).on('load', () =>
 {
-	// get points
+
+	// populate with points
+	for (let i = 0; i < 15; ++i)
+	{
+		const left = Math.random() * 101;
+		const top = Math.random() * 101;
+		$('#landingcontainer').append(`<div class='point' style='left: ${left}%; top: ${top}%'></div>`);
+	}
+
+	// populate points array
 	let points = [];
-	$.each($('#graphic').children(), (i, v) =>
+	$.each($('#landingcontainer').children(), (i, v) =>
 	{
-		points.push(v);
+		if ($(v).hasClass('point'))
+		{
+			points.push(v);
+		}
+	});
+	points.sort(function(a, b)
+	{
+		const atop = parseFloat(a.style.left);
+		const btop = parseFloat(b.style.left);
+		if (atop < btop)
+		{
+			return -1;
+		}
+
+		if (atop > btop)
+		{
+			return 1;
+		}
+		return 0;
 	});
 
-	$.each(points, (i) =>
+	// populate with lines
+	$.each(points, (i, v) =>
 	{
-		const v = points[i];
-		const l = v.style.left;
-		const r = v.style.right;
-		const t = v.style.top;
-		const b = v.style.bottom;
+		let next = points[i + 1];
+		if (next == undefined) next = points[0];
 
-		// positioning
-		const horiz = (l != '') ? 'left' : 'right';
-		const vert = (t != '') ? 'top' : 'bottom';
-		const x = (l != '') ? l : r;
-		const y = (t != '') ? t : b;
+		const shape = v.getBoundingClientRect();
+		const nextShape = next.getBoundingClientRect();
 
-		const next = (points[i + 1] == undefined) ? points[0] : points[i + 1];
+		const diffX = nextShape.left - shape.left;
+		const diffY = nextShape.top - shape.top;
+		const hypotLength = Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2));
 
-		// sizing
-		const diffX = Math.abs(next.getBoundingClientRect().left - v.getBoundingClientRect().left);
-		const diffY = Math.abs(next.getBoundingClientRect().top - v.getBoundingClientRect().top);
-		const longest = Math.max(diffX, diffY);
+		const theta = Math.atan(diffX / diffY);
+		const thetadeg = theta * (180 / Math.PI);
+		let rotation = 0;
+		if (diffY >= 0)
+			rotation = 90 - thetadeg;
+		else
+			rotation = 270 - thetadeg;
 
-		//rotation
-		const rotation = (longest == diffY) ? 90 : 0;
-
-		const a = `<div class='line position-absolute' style='width: ${longest}px; ${horiz}: ${x}; ${vert}: ${y};' style='transform: rotate(${rotation}deg);'></div>`;
-		console.log(a);
-		$('#graphic').append(a);
+		$('#landingcontainer').append(`<div class='line' finalwidth='${hypotLength}' style='top: ${shape.top}px; left: ${shape.left}px; transform: rotate(${rotation}deg);'></div>`);
 	});
+
+	// populate lines array
+	let lines = [];
+	$.each($('#landingcontainer').children(), (i, v) =>
+	{
+		if ($(v).hasClass('line'))
+		{
+			lines.push(v);
+		}
+	});
+	lines.sort(function(a, b)
+	{
+		const atop = parseFloat(a.style.left);
+		const btop = parseFloat(b.style.left);
+		if (atop < btop)
+		{
+			return -1;
+		}
+
+		if (atop > btop)
+		{
+			return 1;
+		}
+		return 0;
+	});
+
+	// resize lines
+	const transitionTime = parseFloat($('.line').css('transition-duration'));
+	for (let i = 0; i < lines.length; ++i)
+	{
+		const v = lines[i];
+		$(v).css({'width': `${$(v).attr('finalwidth')}px`, 'transition-delay': `${transitionTime * i}s`});
+	}
 });
