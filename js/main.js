56805fc7f0acf2a10ff6e457f19a4e314993d1ee
Ryan Stewart <ryanstewartalex@gmail.com>
2020-10-15 15:24:01 -0500

    Redo Breakpoints

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // Ryan Stewart 10/15/20
 const browswertransitionevents = 'transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd';
+const transitionTime = getComputedStyle(document.documentElement).getPropertyValue('--transitionTime');
 const bgcolor = getComputedStyle(document.documentElement).getPropertyValue('--bgcolor');
 const bgcolor2 = getComputedStyle(document.documentElement).getPropertyValue('--bgcolor2');
 
@@ -22,9 +23,11 @@ function changeToContact(e)
 $(document).ready(() =>
 {
     $('#landing').css({'box-shadow': '-8px 8px 0 0 rgba(0, 0, 0, .35)'});
+    $('body').css({'transition': `background-color ${transitionTime.replace(/[^\d.]/g, '') * 2}s`})
 
     $('#headertext').click(() =>
     {
+        if ($('#home').css('display') == 'flex') return;
         $('#content-box').one(browswertransitionevents, changeToHome);
         $('body').css({'background-color': bgcolor});
         $('#content-box').css({'max-height': '0'});
