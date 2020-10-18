5bbbdc7f38511699ba3f8454ab8d4d096d2c8aad
Ryan Stewart <ryanstewartalex@gmail.com>
2020-10-18 15:36:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
-
 const port = 8080;
-app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) =>
-{
+app.use(express.static(__dirname));
+app.set('views', __dirname);
+app.set('view engine', 'jade');
+
+app.get('/', (req, res) => {
 	res.render('index');
-});
+})
 
-app.listen(port, () =>
-{
-	console.log('app running');
-});
+app.listen(port, (err) => {
+	if (err) console.log(err);
+ 	console.log(`App listening at http://localhost:${port}`)
+})
