41a52a272b9cda0c03a00d9f748dd7dfdce51051
Ryan Stewart <ryanstewartalex@gmail.com>
2020-11-04 08:08:48 -0600

    Remove Erraneous Code

--- style.js
+++
@@ -1,9 +0,0 @@
-const square3 = document.querySelector('#square3');
-
-window.addEventListener('resize', (e) =>
-{
-    const pb = square3.style.paddingBottom;
-    const h = square3.style.height;
-    console.log(pb, h);
-
-});
