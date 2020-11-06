c93723ba70a6c8c1c1bf82dbcba05c6802eb30c5
Ryan Stewart <ryanstewartalex@gmail.com>
2020-11-06 05:40:34 -0600

    Add Type

--- website3/main.js
+++ website3/main.js
@@ -1,27 +1,58 @@
-const letterDelay = 300;
-const lineDelay = 2000;
+const letterDelay = 20;
+let h1;
+let cursor;
+let isTyping = false;
 
-function type()
+function type(line, time = 0)
 {
-    const h1 = document.querySelector('h1');
-    const lines =
-    [
-        'Hi! My name is Ryan.',
-        'I like web design.',
-        'Here are some places you can find me.'
-    ];
+    if (isTyping)
+    {
+        const interval = setInterval(() =>
+        {
+            if (!isTyping)
+            {
+                type(line, time);
+                clearInterval(interval);
+            }
+        }, 500);
+        return;
+    }
 
-    lines.forEach((line, i) => {
+    isTyping = true;
+
+    line.split('').forEach((char, j) => {
         setTimeout(() =>
         {
-            line.split('').forEach((char, j) => {
-                setTimeout(() => { h1.textContent += char; }, letterDelay * j);
-            });
-        }, 1000 * i);
+            const cursorCopy = cursor.cloneNode();
+            h1.textContent += char;
+            h1.append(cursorCopy);
+        }, letterDelay * j + time);
     });
+
+    setTimeout(() =>
+    {
+        isTyping = false;
+    }, line.length * letterDelay + time);
+}
+
+function start()
+{
+    h1 = document.querySelector('h1');
+    cursor = document.querySelector('#cursor');
+
+    type('Hi, ');
+    type('so where do I begin with this?', 1000);
+    type('\r\n\r\n', 1000);
+    type('- My name is Ryan.');
+    type('\r\n');
+    type('- I like code.');
+    type('\r\n');
+    type('- I like caffeine.');
+    type('\r\n\r\n', 2000);
+    type('Also, here are some places you can find me:');
 }
 
 window.addEventListener('load', () =>
 {
-    type();
+    start();
 });
