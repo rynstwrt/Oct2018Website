74296087029eacfa43b1e75c928b2bb71b87dafa
Ryan Stewart <ryanstewartalex@gmail.com>
2020-11-08 17:32:22 -0600

    Work on Website 5

--- website5/main.js
+++ website5/main.js
@@ -1,7 +1,5 @@
-function start(container, inputValue)
+function start(container, color)
 {
-	let [raw, h, s, l] = inputValue.match(/hsl\((\d+), (\d+%), (\d+%)\)/);
-	const color = new Color(h, s, l);
 	let colors = [
 		...color.getShades(6),
 		...color.getTints(6),
@@ -20,17 +18,61 @@ function start(container, inputValue)
 	});
 }
 
+function hexToHSL(hex)
+{
+	const r = parseInt(hex.substr(1, 2), 16) / 255;
+	const g = parseInt(hex.substr(3, 2), 16) / 255;
+	const b = parseInt(hex.substr(5, 2), 16) / 255;
+
+	const max = Math.max(r, g, b);
+	const min = Math.min(r, g, b);
+	let h, s, l = (min + max) / 2;
+
+	if (min === max)
+	{
+		h = 0;
+		s = 0;
+	}
+	else
+	{
+		const d = max - min;
+		s = l > .5 ? d / (2 - min - max) : d / (min + max);
+
+		switch(max)
+		{
+			case r:
+				h = (g - b) / d + (g < b ? 6 : 0);
+				break;
+
+			case g:
+				h = (b - r) / d + 2;
+				break;
+
+			case b:
+				h = (r - g) / d + 4;
+				break;
+		}
+		h /= 6;
+	}
+
+	return [h * 360, s * 100, l * 10];
+}
+
 window.addEventListener('load', () =>
 {
 	const input = document.querySelector('#color-input');
 	const colorContainer = document.querySelector('#color-container');
 
-	start(colorContainer, input.value);
+	let [h, s, l] = hexToHSL(input.value);
+
+	const color = new Color(h, s, l);
+
+	start(colorContainer, color);
 
 	input.addEventListener('input', () =>
 	{
 		while (colorContainer.firstChild)
 			colorContainer.firstChild.remove();
-		start(colorContainer, input.value);
+		start(colorContainer, color);
 	});
 });
