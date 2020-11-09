6203b5ec40c2b2cbf0779a76138a4a0963dba580
Ryan Stewart <ryanstewartalex@gmail.com>
2020-11-08 23:41:28 -0600

    Rewrite color.js

--- website5/color.js
+++ website5/color.js
@@ -1,72 +1,85 @@
 class Color
 {
-	constructor(hue, sat, lum)
+	constructor(...args)
 	{
-		this.hue = hue;
-		this.sat = parseFloat(sat);
-		this.lum = parseFloat(lum);
-	}
-
-	getShades(steps)
-	{
-		let shades = [];
-		const step = this.lum / steps;
-		for (let i = 0; i < steps; ++i)
+		if (args.length === 1) // if hex
 		{
-			shades.push([this.hue, this.sat, step * i]);
+			const hex = args[0];
+			const r = parseInt(hex.substr(1, 2), 16) / 255;
+			const g = parseInt(hex.substr(3, 2), 16) / 255;
+			const b = parseInt(hex.substr(5, 2), 16) / 255;
+
+			const max = Math.max(r, g, b);
+			const min = Math.min(r, g, b);
+			let h, s, l = (min + max) / 2;
+
+			if (min === max)
+			{
+				h = 0;
+				s = 0;
+			}
+			else
+			{
+				const d = max - min;
+				s = l > .5 ? d / (2 - min - max) : d / (min + max);
+
+				switch(max)
+				{
+					case r:
+						h = (g - b) / d + (g < b ? 6 : 0);
+						break;
+
+					case g:
+						h = (b - r) / d + 2;
+						break;
+
+					case b:
+						h = (r - g) / d + 4;
+						break;
+				}
+				h /= 6;
+			}
+
+			this.hue = h * 360;
+			this.sat = s * 100;
+			this.lum = l * 100;
 		}
-		return shades;
-	}
-
-	getTints(steps)
-	{
-		let tints = [];
-		const step = (100 - this.lum) / steps;
-		for (let i = 0; i < steps; ++i)
+		else // if HSL
 		{
-			tints.push([this.hue, this.sat, this.lum + step * i]);
-		}
-		return tints;
-	}
-
-	getComplimentary()
-	{
-		return [this.hue + 180, this.sat, this.lum];
-	}
-
-	getSplitComplementary(range)
-	{
-		return [[this.hue + range, this.sat, this.lum],
-		[this.hue + 180, this.sat, this.lum],
-		[this.hue - range, this.sat, this.lum]];
-	}
-
-	getDoubleSplitComplementary(range)
-	{
-		return [[this.hue + range, this.sat, this.lum],
-		[this.hue + 180 - range, this.sat, this.lum],
-		[this.hue + 180 + range, this.sat, this.lum],
-		[this.hue - range, this.sat, this.lum]];
-	}
-
-	getAnalagous(range, steps)
-	{
-		let colors = [];
-		const minH = this.hue - range;
-		const maxH = this.hue + range;
-		const step = (maxH - minH) / steps;
-
-
-		for (let i = 0; i < steps; ++i)
-		{
-			colors.push([step * i, this.sat, this.lum]);
+			this.hue = args[0];
+			this.sat = args[1];
+			this.lum = args[2];
 		}
 
-		return colors;
 	}
 
-	getTriadic()
+	getHSL()
 	{
-		return [[this.hue - 120, this.sat, this.lum], [this.hue, this.sat, this.lum], [this.hue + 120, this.sat, this.lum]];
+		return `hsl(${this.hue}, ${this.sat}%, ${this.lum}%)`;
+	}
+
+	darken(percent)
+	{
+		return new Color(this.hue, this.sat, this.lum * (1 - percent / 100));
+	}
+
+	lighten(percent)
+	{
+		return new Color(this.hue, this.sat, this.lum * (1 + percent / 100));
+	}
+
+	rotateHue(percent)
+	{
+		return new Color(this.hue + (360 * (percent / 100)), this.sat, this.lum);
+	}
+
+	addHue(hue)
+	{
+		return new Color(this.hue + hue, this.sat, this.lum);
+	}
+
+	subtractHue(hue)
+	{
+		return new Color(this.hue - hue, this.sat, this.lum);
 	}
 }
--- website5/main.js
+++ website5/main.js
@@ -1,61 +1,42 @@
-function start(container, color)
+function start(container, hex)
 {
-	let colors = [
-		...color.getShades(6),
-		...color.getTints(6),
-		color.getComplimentary(),
-		...color.getSplitComplementary(30),
-		...color.getDoubleSplitComplementary(30),
-		...color.getAnalagous(30, 6),
-		...color.getTriadic()
+
+	let color = new Color(hex);
+
+	const colors = [
+		color.getHSL(),
+		color.darken(50).getHSL(),
+		color.lighten(50).getHSL(),
+		color.rotateHue(10).getHSL(),
+		color.addHue(180).getHSL(),
+		color.subtractHue(180).getHSL()
 	];
 
-	colors.forEach((c, i) => {
-		const box = document.createElement('div');
-		box.classList.add('color-box');
-		box.style.backgroundColor = `hsl(${c[0]}, ${c[1]}%, ${c[2]}%)`;
-		container.append(box);
+	const boxes = getBoxes(container, colors.length);
+	boxes.forEach((box, i) =>
+	{
+		box.style.backgroundColor = colors[i];
 	});
 }
 
-function hexToHSL(hex)
+function getBoxes(container, amount)
 {
-	const r = parseInt(hex.substr(1, 2), 16) / 255;
-	const g = parseInt(hex.substr(3, 2), 16) / 255;
-	const b = parseInt(hex.substr(5, 2), 16) / 255;
+	const currentBoxes = document.querySelectorAll('.color-box');
 
-	const max = Math.max(r, g, b);
-	const min = Math.min(r, g, b);
-	let h, s, l = (min + max) / 2;
+	if (currentBoxes.length === amount) return currentBoxes;
 
-	if (min === max)
+	while (container.firstChild) container.firstChild.remove();
+
+	let boxes = [];
+	for (let i = 0; i < amount; ++i)
 	{
-		h = 0;
-		s = 0;
-	}
-	else
-	{
-		const d = max - min;
-		s = l > .5 ? d / (2 - min - max) : d / (min + max);
-
-		switch(max)
-		{
-			case r:
-				h = (g - b) / d + (g < b ? 6 : 0);
-				break;
-
-			case g:
-				h = (b - r) / d + 2;
-				break;
-
-			case b:
-				h = (r - g) / d + 4;
-				break;
-		}
-		h /= 6;
+		const box = document.createElement('div');
+		box.classList.add('color-box');
+		boxes.push(box);
+		container.append(box);
 	}
 
-	return [h * 360, s * 100, l * 10];
+	return boxes;
 }
 
 window.addEventListener('load', () =>
@@ -63,16 +44,6 @@ window.addEventListener('load', () =>
 	const input = document.querySelector('#color-input');
 	const colorContainer = document.querySelector('#color-container');
 
-	let [h, s, l] = hexToHSL(input.value);
-
-	const color = new Color(h, s, l);
-
-	start(colorContainer, color);
-
-	input.addEventListener('input', () =>
-	{
-		while (colorContainer.firstChild)
-			colorContainer.firstChild.remove();
-		start(colorContainer, color);
-	});
+	start(colorContainer, input.value);
+	input.addEventListener('input', () => start(colorContainer, input.value));
 });
