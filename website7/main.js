eb6104b21f27c55d34110726e326696b86a63b62
Ryan Stewart <ryanstewartalex@gmail.com>
2020-11-09 07:33:13 -0600

    Start Website 7

---
+++ website7/main.js
@@ -0,0 +1,56 @@
+function createScene(canvas, engine)
+{
+	const scene = new BABYLON.Scene(engine);
+	const camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(-10, 10, 30), scene);
+	camera.setTarget(BABYLON.Vector3.Zero());
+	camera.attachControl(canvas, false);
+	//camera.inputs.clear();
+
+	const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(-10, 10, 30), scene);
+
+	const boxSize = 3;
+	const points = getPoints(50, boxSize);
+	for (let i = 0; i < points.length; ++i)
+	{
+		const point = points[i];
+
+		const box = BABYLON.MeshBuilder.CreateBox('box', { width: boxSize, height: boxSize, depth: boxSize }, scene);
+		box.position = point;
+	}
+
+	return scene;
+}
+
+function getPoints(amount, cubeSize)
+{
+	const points = [];
+	const margin = 1;
+
+	for (let i = -amount / 2; i < amount / 2; ++i)
+	{
+		for (let j = -amount / 2; j < amount / 2; ++j)
+		{
+			const z = Math.random() * cubeSize;
+			points.push(new BABYLON.Vector3(i * (cubeSize + margin), j * (cubeSize + margin), z));
+		}
+	}
+
+	return points;
+}
+
+window.addEventListener('load', () =>
+{
+	const canvas = document.querySelector('canvas');
+	const engine = new BABYLON.Engine(canvas, true);
+	const scene = createScene(canvas, engine);
+
+	engine.runRenderLoop(() =>
+	{
+		scene.render();
+	});
+});
+
+window.addEventListener('resize', () =>
+{
+	engine.resize();
+});
