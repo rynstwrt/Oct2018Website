9c7d495bc5946e49768523986e28473433886d6f
Ryan Stewart <ryanstewartalex@gmail.com>
2020-11-11 23:34:06 -0600

    Add CDN

--- website7/main.js
+++ website7/main.js
@@ -2,46 +2,36 @@
 function createScene(canvas, engine) {
     /* Scene */
     var scene = new BABYLON.Scene(engine);
-    /* Camera */
-    var camera = new BABYLON.ArcRotateCamera('cam', Math.PI / 4, Math.PI / 3, 300, BABYLON.Vector3.Zero(), scene);
-    camera.attachControl(canvas, true);
     /* Lights */
     new BABYLON.PointLight('pl', new BABYLON.Vector3(100, 200, 0), scene);
-    new BABYLON.HemisphericLight("hemilight", new BABYLON.Vector3(0, 1, 0), scene);
-    /* Ground */
-    var groundWidth = 100;
-    var groundHeight = groundWidth;
-    var ground = BABYLON.MeshBuilder.CreateGround('ground', { width: groundWidth, height: groundHeight }, scene);
-    // const groundMat = new BABYLON.StandardMaterial('groundmat', scene);
-    // groundMat.diffuseColor = BABYLON.Color3.FromHexString('#52433c');
-    // ground.material = groundMat;
+    new BABYLON.HemisphericLight("hemilight1", new BABYLON.Vector3(0, 1, 0), scene);
+    //new BABYLON.HemisphericLight("hemilight2", new BABYLON.Vector3(0, -1, 0), scene);
     /* SolidParticleSystem */
     var sps = new BABYLON.SolidParticleSystem('sps', scene, { updatable: false });
     var particle = BABYLON.MeshBuilder.CreateBox('particle', {}, scene);
-    var numParticles = 100;
-    var particleWidth = 10;
-    var minParticleHeight = particleWidth / 2;
-    var maxParticleHeight = particleWidth * 5;
-    var startingPoint = new BABYLON.Vector3(-groundWidth / 2 + particleWidth / 2, 0, -groundHeight / 2 + particleWidth / 2);
+    var numParticles = 1000;
+    var rowCount = Math.floor(Math.sqrt(numParticles));
+    var particleWidth = 2;
+    var particles = [];
     sps.addShape(particle, numParticles, { positionFunction: function (p, i) {
-            var height = Math.random() * (maxParticleHeight - minParticleHeight) + minParticleHeight;
+            var row = Math.floor(i / rowCount);
+            var col = i % rowCount;
+            var height = Math.sin(i) * 25;
+            var x = col * particleWidth;
+            var z = row * particleWidth;
+            p.position = new BABYLON.Vector3(x, 0, z);
             p.scaling = new BABYLON.Vector3(particleWidth, height, particleWidth);
-            // const xMin = -groundWidth / 2 + particleWidth / 2;
-            // const xMax = groundWidth / 2 - particleWidth / 2;
-            // const zMin = -groundHeight / 2 + particleWidth / 2;
-            // const zMax = groundHeight / 2 - particleWidth / 2;
-            //
-            // p.position = new BABYLON.Vector3(
-            // 	Math.random() * (xMax - xMin) + xMin,
-            // 	height / 2,
-            // 	Math.random() * (zMax - zMin) + zMin);
-            var rowCount = Math.floor(Math.sqrt(numParticles));
             var particleColor3 = BABYLON.Color3.FromHexString('#ff6b6b');
             var particleColor4 = BABYLON.Color4.FromColor3(particleColor3);
             p.color = particleColor4;
+            particles.push(p);
         } });
     sps.buildMesh();
     particle.dispose();
+    var camTarget = particles[Math.floor((particles.length - 1) / 2)].position;
+    /* Camera */
+    var camera = new BABYLON.ArcRotateCamera('cam', Math.PI / 4, Math.PI / 3, 300, camTarget, scene);
+    camera.attachControl(canvas, true);
     return scene;
 }
 window.addEventListener('load', function () {
--- website7/main.ts
+++ website7/main.ts
@@ -3,58 +3,48 @@ function createScene(canvas: HTMLCanvasElement, engine: BABYLON.Engine)
 	/* Scene */
 	const scene = new BABYLON.Scene(engine);
 
-	/* Camera */
-	const camera = new BABYLON.ArcRotateCamera('cam', Math.PI / 4, Math.PI / 3, 300, BABYLON.Vector3.Zero(), scene);
-	camera.attachControl(canvas, true);
-
 	/* Lights */
 	new BABYLON.PointLight('pl', new BABYLON.Vector3(100, 200, 0), scene);
-	new BABYLON.HemisphericLight("hemilight", new BABYLON.Vector3(0, 1, 0), scene);
-
-	/* Ground */
-	const groundWidth = 100;
-	const groundHeight = groundWidth;
-	const ground = BABYLON.MeshBuilder.CreateGround('ground', { width: groundWidth, height: groundHeight }, scene);
-
-	// const groundMat = new BABYLON.StandardMaterial('groundmat', scene);
-	// groundMat.diffuseColor = BABYLON.Color3.FromHexString('#52433c');
-	// ground.material = groundMat;
+	new BABYLON.HemisphericLight("hemilight1", new BABYLON.Vector3(0, 1, 0), scene);
+	//new BABYLON.HemisphericLight("hemilight2", new BABYLON.Vector3(0, -1, 0), scene);
 
 	/* SolidParticleSystem */
 	const sps = new BABYLON.SolidParticleSystem('sps', scene, {updatable: false});
 	const particle = BABYLON.MeshBuilder.CreateBox('particle', {}, scene);
-	const numParticles = 100;
-	const particleWidth = 10;
-	const minParticleHeight = particleWidth / 2;
-	const maxParticleHeight = particleWidth * 5;
-	const startingPoint = new BABYLON.Vector3(-groundWidth / 2 + particleWidth / 2, 0, -groundHeight / 2 + particleWidth / 2);
+	const numParticles = 1000;
+	const rowCount = Math.floor(Math.sqrt(numParticles));
+	const particleWidth = 2;
 
+	const particles = [];
 	sps.addShape(particle, numParticles, { positionFunction: (p: BABYLON.SolidParticle, i: number) =>
 	{
-		const height = Math.random() * (maxParticleHeight - minParticleHeight) + minParticleHeight;
+		const row = Math.floor(i / rowCount);
+		const col = i % rowCount;
+		const height = Math.sin(i) * 25;
+
+		const x = col * particleWidth;
+		const z = row * particleWidth
+		p.position = new BABYLON.Vector3(x, 0, z);
+
 		p.scaling = new BABYLON.Vector3(particleWidth, height, particleWidth);
-
-		// const xMin = -groundWidth / 2 + particleWidth / 2;
-		// const xMax = groundWidth / 2 - particleWidth / 2;
-		// const zMin = -groundHeight / 2 + particleWidth / 2;
-		// const zMax = groundHeight / 2 - particleWidth / 2;
-		//
-		// p.position = new BABYLON.Vector3(
-		// 	Math.random() * (xMax - xMin) + xMin,
-		// 	height / 2,
-		// 	Math.random() * (zMax - zMin) + zMin);
-
-		const rowCount = Math.floor(Math.sqrt(numParticles));
-
-
 
 		const particleColor3 = BABYLON.Color3.FromHexString('#ff6b6b');
 		const particleColor4 = BABYLON.Color4.FromColor3(particleColor3);
 		p.color = particleColor4;
+
+		particles.push(p);
 	}});
 
 	sps.buildMesh();
 	particle.dispose();
+
+	const camTarget = particles[Math.floor((particles.length - 1) / 2)].position;
+
+	/* Camera */
+	const camera = new BABYLON.ArcRotateCamera('cam',
+	Math.PI / 4, Math.PI / 3, 300,
+	camTarget, scene);
+	camera.attachControl(canvas, true);
 
 	return scene;
 }
