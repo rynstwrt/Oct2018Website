281599d5ff88790745f33475ca6ac93b1380f04d
Ryan Stewart <ryanstewartalex@gmail.com>
2020-11-14 22:54:31 -0600

    Update Websites Website

--- websites/main.js
+++ websites/main.js
@@ -1,19 +1,19 @@
 "use strict";
-// diffuse: basic color of object
-// groundColor: light in opposite direction color
-// specular: highlight color of object
 function createScene(canvas, engine) {
     // Scene
     var scene = new BABYLON.Scene(engine);
     scene.clearColor = new BABYLON.Color4(0, 0, 0, 0);
-    var box = BABYLON.MeshBuilder.CreateBox('box', { size: 10 }, scene);
-    box.position = BABYLON.Vector3.Zero();
-    // Light
-    var light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 1, 0), scene);
+    // Shape
+    var shape = BABYLON.MeshBuilder.CreateIcoSphere('shape', { radius: 25, subdivisions: 2 }, scene);
+    var shapeMat = new BABYLON.StandardMaterial('shapemat', scene);
+    shapeMat.emissiveColor = BABYLON.Color3.FromHexString('#FEC7A5');
+    shapeMat.wireframe = true;
+    shape.material = shapeMat;
     // Camera
     var camera = new BABYLON.ArcRotateCamera('camera', 0, Math.PI / 2, 300, BABYLON.Vector3.Zero(), scene);
     camera.attachControl(canvas, false);
-    var cameraAnim = new BABYLON.Animation('cameraanim', 'alpha', 60, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_RELATIVE);
+    var cameraAnimY = new BABYLON.Animation('cameraanimy', 'alpha', 60, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_RELATIVE);
+    var cameraAnimX = new BABYLON.Animation('cameraanimx', 'beta', 60, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_RELATIVE);
     var keys = [];
     keys.push({
         frame: 0,
@@ -23,9 +23,10 @@ function createScene(canvas, engine) {
         frame: 120,
         value: Math.PI / 2
     });
-    cameraAnim.setKeys(keys);
-    camera.animations.push(cameraAnim);
-    scene.beginAnimation(camera, 0, 120, true);
+    cameraAnimY.setKeys(keys);
+    cameraAnimX.setKeys(keys);
+    camera.animations = [cameraAnimY, cameraAnimX];
+    scene.beginAnimation(camera, 0, 120, true, .25);
     // Return
     return scene;
 }
--- websites/main.ts
+++ websites/main.ts
@@ -1,24 +1,28 @@
-// diffuse: basic color of object
-// groundColor: light in opposite direction color
-// specular: highlight color of object
 function createScene(canvas: HTMLCanvasElement, engine: BABYLON.Engine)
 {
 	// Scene
 	const scene = new BABYLON.Scene(engine);
 	scene.clearColor = new BABYLON.Color4(0, 0, 0, 0);
 
-	const box = BABYLON.MeshBuilder.CreateBox('box', { size: 10 }, scene);
-	box.position = BABYLON.Vector3.Zero();
+	// Shape
+	const shape = BABYLON.MeshBuilder.CreateIcoSphere('shape',
+	{ radius: 25, subdivisions: 2 }, scene);
 
-	// Light
-	const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 1, 0), scene);
+	const shapeMat = new BABYLON.StandardMaterial('shapemat', scene);
+	shapeMat.emissiveColor = BABYLON.Color3.FromHexString('#FEC7A5');
+	shapeMat.wireframe = true;
+	shape.material = shapeMat;
 
 	// Camera
 	const camera = new BABYLON.ArcRotateCamera('camera', 0, Math.PI / 2, 300, BABYLON.Vector3.Zero(), scene);
 	camera.attachControl(canvas, false);
 
-	const cameraAnim = new BABYLON.Animation('cameraanim', 'alpha', 60,
+	const cameraAnimY = new BABYLON.Animation('cameraanimy', 'alpha', 60,
 	BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_RELATIVE);
+
+	const cameraAnimX = new BABYLON.Animation('cameraanimx', 'beta', 60,
+	BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_RELATIVE);
+
 	const keys = [];
 	keys.push({
 		frame: 0,
@@ -28,9 +32,11 @@ function createScene(canvas: HTMLCanvasElement, engine: BABYLON.Engine)
 		frame: 120,
 		value: Math.PI / 2
 	});
-	cameraAnim.setKeys(keys);
-	camera.animations.push(cameraAnim);
-	scene.beginAnimation(camera, 0, 120, true);
+
+	cameraAnimY.setKeys(keys);
+	cameraAnimX.setKeys(keys);
+	camera.animations = [cameraAnimY, cameraAnimX];
+	scene.beginAnimation(camera, 0, 120, true, .25);
 
 	// Return
 	return scene;
