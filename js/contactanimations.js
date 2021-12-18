2068e573c06808fa204071171e769055242ee735
Ryan Stewart <ryanstewartalex@gmail.com>
2021-12-18 13:07:53 -0600

    Fix Portrait Tablet Support and Fix Animations

--- js/contactanimations.js
+++ js/contactanimations.js
@@ -1,16 +1,3 @@
-// const contactAnim = anime.timeline({ loop: true, autoplay: false, easing: "easeOutExpo", duration: 100 })
-//     .add({
-//         targets: "#contact-page li a",
-//         scale: [1, 1.1],
-//         delay: anime.stagger(50)
-//     })
-//     .add({
-//         targets: "#contact-page li a",
-//         scale: [1.1, 1],
-//         delay: anime.stagger(50)
-//     });
-
-
 const contactAnim = anime({
     targets: "#social-list-container a",
     color: ["#111111", "#EF8354"],
