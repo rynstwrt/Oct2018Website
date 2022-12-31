9a2d003344455c3b6ad34efbfff0d21d87384306
Ryan Stewart <ryanstewartalex@gmail.com>
2022-12-30 22:21:38 -0600

    Finish Adding Konami Code

--- js/konamicode.js
+++ js/konamicode.js
@@ -23,5 +23,23 @@ function onKonamiCode()
 {
     console.log("KONAMI CODE ENTERED! A winner is you!");
 
+    const container = document.createElement("div");
+    container.id = "konami-container";
+    document.body.appendChild(container);
+    document.body.style.overflowY = "hidden";
 
+    const flashingText = document.createElement("h3");
+    flashingText.id = "konami-text";
+    flashingText.textContent = "A WINNER IS YOU";
+    container.appendChild(flashingText);
+
+    // create "Y'all mind if I praise the lord?" gif
+    const danceGif = document.createElement("img");
+    danceGif.src = "assets/dance.gif";
+    container.appendChild(danceGif);
+
+    // create left shark gif
+    const sharkGif = document.createElement("img");
+    sharkGif.src = "assets/leftshark.gif";
+    container.appendChild(sharkGif);
 }
